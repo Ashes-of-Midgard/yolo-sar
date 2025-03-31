0d195f0663ea2f6d6d4554b18310b5f7df0e73e3
yyl <bhyyl@buaa.edu.cn>
2025-03-31 14:34:23 +0000

    Fix: change the implementation of AdvTrainer for DDP env & no longer support YOLOAdvDn

--- train.py
+++ train.py
@@ -3,12 +3,12 @@ from ultralytics import YOLO, YOLOAdv, YOLOAdvDn
 
 if __name__=="__main__":
     # 加载预训练的yolo12权重
-    model = YOLO("yolo12m-ppa.yaml", task="detect")
-    check_point_model = YOLO("yolo12m.pt")
+    model = YOLOAdv("yolo12n.yaml", task="detect")
+    check_point_model = YOLO("yolo12n.pt")
     model.load_state_dict(check_point_model.state_dict(), strict=False)
 
     # 用自定义的SAR数据集微调
-    results = model.train(data="datasets/OGSOD_yolo/dataset.yaml", epochs=100)
+    results = model.train(data="datasets/SSDD_yolo/dataset.yaml", epochs=100)
 
     # 评测模型的效果
     results = model.val()
