2f4db2ce1fbb72ebffc9c96c13dbab49b61c7ec1
yyl <bhyyl@buaa.edu.cn>
2025-04-01 13:11:25 +0000

    remove un-used code in lsk

--- ultralytics/nn/modules/lsk.py
+++ ultralytics/nn/modules/lsk.py
@@ -12,27 +12,6 @@ class DWConv(nn.Module):
         return x
 
 
-class Mlp(nn.Module):
-    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
-        super().__init__()
-        out_features = out_features or in_features
-        hidden_features = hidden_features or in_features
-        self.fc1 = nn.Conv2d(in_features, hidden_features, 1)
-        self.dwconv = DWConv(hidden_features)
-        self.act = act_layer()
-        self.fc2 = nn.Conv2d(hidden_features, out_features, 1)
-        self.drop = nn.Dropout(drop)
-
-    def forward(self, x):
-        x = self.fc1(x)
-        x = self.dwconv(x)
-        x = self.act(x)
-        x = self.drop(x)
-        x = self.fc2(x)
-        x = self.drop(x)
-        return x
-
-
 class Attention(nn.Module):
     def __init__(self, d_model):
         super().__init__()
